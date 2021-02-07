b07c3cdb85dafe09eeec1cb8f9d5308c1a3070ce
hiteshlala <hiteshlala@gmail.com>
2021-02-06 19:26:23 -0800

    First working version with localhost
    Implements:
    - Login and saving of session cookie.
    - Logout and clearing of session cookie.
    - Auto searching for current url after login or if already logged in.
    - Searching for passwords.
    - Paging through pottential matches.
    
    TODO:
    - Edit existing record.
    - Delete existing record.
    - Create new record.
    - CSS for each panel.
    
     Changes to be committed:
    	modified:   background.js
    	modified:   manifest.json
    	modified:   popup.css
    	modified:   popup.html
    	modified:   popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,129 @@
+const server = 'http://localhost:7000';
+let passwords = [];
+let auth;
+let index = 0;
 
-async function setup() {}
+const loading = document.getElementById( 'loading' );
+const logout = document.getElementById( 'logout' );
+const submitlogout = document.getElementById( 'submitlogout' );
+const login = document.getElementById( 'login' );
+const submitlogin = document.getElementById( 'submitlogin' );
+const username = document.getElementById( 'username' );
+const password = document.getElementById( 'password' );
+const loginmsg = document.getElementById( 'loginmsg' );
+const message = document.getElementById( 'message' );
+const search = document.getElementById( 'search' );
+const results = document.getElementById( 'results' );
+const query = document.getElementById( 'query' );
+const left = document.getElementById( 'left' );
+const right = document.getElementById( 'right' );
+const page = document.getElementById( 'page' );
+const item = document.getElementById( 'item' );
+const clear = document.getElementById( 'clear' );
 
-async function login() {}
+const ititle = document.getElementById( 'i-title' );
+const iurl = document.getElementById( 'i-url' );
+const iusername = document.getElementById( 'i-username' );
+const ipassword = document.getElementById( 'i-password' );
+const iaccountno = document.getElementById( 'i-accountno' );
+const ichallenge1 = document.getElementById( 'i-challenge1' );
+const ichallenge2 = document.getElementById( 'i-challenge2' );
+const iemail = document.getElementById( 'i-email' );
+const inotestext = document.getElementById( 'i-notes-text' );
 
-async function getList() {}
 
+function getRequest( url ) {
+  return new Promise(( resolve, reject ) => {
+    let req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open( 'GET', url, true );
+    auth && req.setRequestHeader( 'Authorization', auth );
+    req.onload = r => {
+      if( r.target.status >= 400 ) {
+        reject( r.target.response );
+      }
+      else {
+        resolve( req.response );
+      }
+    }
+    req.onerror = e => {
+      reject( e );
+    }
+    req.send();
+  });
+}
+
+function postRequest( url, data = {} ) {
+  let send = JSON.stringify( data );
+  return new Promise(( resolve, reject ) => {
+    let req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open( 'POST', url, true );
+    auth && req.setRequestHeader( 'Authorization', auth );
+    req.setRequestHeader( 'Content-Type', 'application/json;charset=UTF-8' );
+    req.setRequestHeader( 'Accept', 'application/json' );
+    req.onload = r => {
+      if( r.target.status >= 400 ) {
+        reject( r.target.response );
+      }
+      else {
+        resolve( req.response );
+      }
+    }
+    req.onerror = e => {
+      reject( e );
+    }
+    req.send( send );
+  });
+}
+
+function deleteRequest( url, data = {} ) {
+  let send = JSON.stringify( data );
+  return new Promise(( resolve, reject ) => {
+    let req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open( 'DELETE', url, true );
+    auth && req.setRequestHeader( 'Authorization', auth );
+    req.setRequestHeader( 'Content-Type', 'application/json;charset=UTF-8' );
+    req.setRequestHeader( 'Accept', 'application/json' );
+    req.onload = r => {
+      if( r.target.status >= 400 ) {
+        reject( r.target.response );
+      }
+      else {
+        resolve( req.response );
+      }
+    }
+    req.onerror = e => {
+      reject( e );
+    }
+    req.send( send );
+  });
+}
+
+function patchRequest( url, data = {} ) {
+  let send = JSON.stringify( data );
+  return new Promise(( resolve, reject ) => {
+    let req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open( 'PATCH', url, true );
+    auth && req.setRequestHeader( 'Authorization', auth );
+    req.setRequestHeader( 'Content-Type', 'application/json;charset=UTF-8' );
+    req.setRequestHeader( 'Accept', 'application/json' );
+    req.onload = r => {
+      if( r.target.status >= 400 ) {
+        reject( r.target.response );
+      }
+      else {
+        resolve( req.response );
+      }
+    }
+    req.onerror = e => {
+      reject( e );
+    }
+    req.send( send );
+  });
+}
 
 async function getUrl() {
   try {
@@ -34,37 +153,154 @@ async function getUrl() {
   }
   catch(e) {
     console.error(e);
+    return '';
   }
 }
 
+async function setup() {
+  try {
+    auth = await new Promise(resolve => chrome.storage.local.get( [ 'key' ], r => resolve(r.key) ));
+    const init = await getRequest(`${server}/api/init`);
+    loading.classList.add( 'hide' );
+    if ( init.loggedIn ) {
+      const q = await getUrl();
+      const r = await getRequest( `${server}/api/passwords${q ? `?filter=${q}` : ''}` );
+      passwords = r.passwords;
+      query.value = q;
+      logout.classList.remove( 'hide' );
+      search.classList.remove( 'hide' );
+      results.classList.remove( 'hide' );
+      populateItem();
+    }
+    else {
+      login.classList.remove( 'hide' );
+    }
+  }
+  catch(e) {
+    console.error( e );
+    loading.classList.add( 'hide' );
+    message.innerText = `${e.message || e}`;
+    message.classList.remove( 'hide' )
+  }
+}
 
-// onOpenPopup().catch(console.error )
+function populateItem() {
+  const i = passwords[ index ];
+  page.innerText = `${index + 1} of ${passwords.length}`;
+  ititle.value = i.title;
+  iurl.value = i.url;
+  iusername.value = i.username;
+  ipassword.value = i.password;
+  iaccountno.value = i.accountno;
+  ichallenge1.value = i.challenge1;
+  ichallenge2.value = i.challenge2;
+  iemail.value = i.email;
+  inotestext.value = i.notes;
+
+} 
+
+async function querychange() {
+  try {
+    const r = await getRequest( `${server}/api/passwords?filter=${query.value}` );
+    passwords = r.passwords;
+    index = 0;
+    populateItem();
+  }
+  catch(e) {
+    console.error(e);
+    message.innerText = `${e.message || e }`;
+    message.classList.remove( 'hide' );
+  }
+}
+
+submitlogin.addEventListener( 'click', async () => {
+  try {
+    const r = await postRequest( `${server}/api/login`, { username: username.value, password: password.value });
+    if ( r.loggedIn ) {
+      auth = r.key;
+      await new Promise( resolve => chrome.storage.local.set( { key: r.key }, resolve ) );
+      login.classList.add( 'hide' );
+      logout.classList.remove( 'hide' );
+      password.value = '';
+      search.classList.remove( 'hide' );
+      results.classList.remove( 'hide' );
+    } 
+    else {
+      loginmsg.innerText = `${r.message}`;
+    }
+  }
+  catch( e ) {
+    console.error(e);
+    loginmsg.innerText = `${e.message || e}`;
+  }
+});
+
+submitlogout.addEventListener( 'click', async () => {
+  try {
+    loading.classList.remove( 'hide' );
+    await deleteRequest( `${server}/api/login`, {});
+    loading.classList.add( 'hide' );
+    await new Promise( resolve => chrome.storage.local.set( { key: '' }, resolve ) );
+  }
+  catch( e ) {
+    console.error(e);
+  }
+  login.classList.remove( 'hide' );
+  logout.classList.add( 'hide' );
+  search.classList.add( 'hide' );
+  results.classList.add( 'hide' );
+});
+
+query.addEventListener( 'input', querychange);
+
+left.addEventListener( 'click', async () => {
+  try {
+    if ( index > 0 ) {
+      index--;
+      populateItem();
+    }
+  }
+  catch( e ) {
+    console.error(e);
+  }
+});
+
+right.addEventListener( 'click', async () => {
+  try {
+    if ( index < passwords.length - 1 ) {
+      index++;
+      populateItem();
+    }
+  }
+  catch( e ) {
+    console.error(e);
+  }
+});
+
+clear.addEventListener( 'click', async () => {
+  try {
+    query.value = '';
+    querychange();
+  }
+  catch( e ) {
+    console.error(e);
+  }
+});
 
 
 
 
 
-// Initialize butotn with users's prefered color
-// let changeColor = document.getElementById("changeColor");
+window.onload = () => {
+  setup().catch(console.error );
+};
 
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+/*
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
+ <!-- <div className={css.but}>
+          <input type="button" value="Cancel" onClick={handleCancel}/>
+          { props.item && <input type="button" value="Delete" onClick={handleDelete}/>}
+          <input type="button" disabled={!canSubmit} value="Submit" onClick={handleSubmit}/>
+        </div> -->
+        */
